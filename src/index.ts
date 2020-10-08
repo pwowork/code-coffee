0ab7fedeb2774cd3a6fc7543d77d11c4657278b0
Peter Oboukhovski <petero@mangodsp.com>
2020-10-08 22:46:37 +0300

    typescript objects

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-/*
-    for (const child of document.getElementById("dogs-list").children){
-        child.addEventListener('click',()=> {
-            alert(child.textContent+ " 1");
-        }) ;
-    }
-    */
-    document.getElementById("dogs-list").childNodes.forEach((child) => {
-        child.addEventListener('click',()=> {
-            alert(child.textContent+ " 12");
-        
-    })
+interface Dog{
+    name:string;
+    age:number;
+}
+
+const dogs : Array<Dog> = [];
+dogs.push({name:"Sammy", age: 2});
+dogs.push({name:"Doggy", age:5});
+dogs.push({name:"Roscoe", age: 7});
+
+dogs.forEach((dog)=>
+{
+    const dogUI = document.createElement("a" );
+    dogUI.href="#";
+    dogUI.className="list-group-item list-group-item-action";
+    dogUI.text = dog.name;
+    dogUI.addEventListener('click',()=> {
+        alert(dog.name+ " "+dog.age);
+    
 });
+    document.getElementById("dogs-list").append(dogUI);
+} );
+
