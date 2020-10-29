94d75f930446362dc9e065d59c0fc8aa9f130efb
Peter Oboukhovski <petero@mangodsp.com>
2020-10-29 19:57:54 +0200

    implement POST method to register dog

--- api/dogs/index.ts
+++ api/dogs/index.ts
@@ -1,13 +1,27 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import {Dog} from "../../shared/interfaces.d"
 import * as mongoose from 'mongoose';
+import { createDocumentRegistry } from "typescript";
 
 mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING,{useNewUrlParser:true});
 
 // mongodb schema
 const dogSchema = new mongoose.Schema({
-    name:String,
-    age:Number
+    name:{
+        type:String,
+        minlength:3,
+        maxlength:16,
+        trim:true,
+        match:/[A-Za-z]*/,
+        required:true,
+        index:true,
+        unique:true
+    },
+    age:{
+        type:Number,
+        min:0,
+        max:20
+    }
 })
 
 // local document data type
@@ -38,6 +52,27 @@ const DogModel = mongoose.model<DogElement>('Dog', dogSchema);
 }
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('Returned list of dogs.');
+    switch(req.method)
+    {
+        case 'POST':
+            await createDog(context);
+            break;
+        case 'GET':
+            await getAllDogs(context);
+            break;
+    }
+};
+async function createDog(context: Context) {
+    const newDog = context.req.body as Dog;
+    const dataDog=await DogModel.create(newDog);
+    context.res={
+        body:dataDog,
+        status:201,
+        header: { 'Content-Type': 'application/json' }
+    };
+}
+
+async function getAllDogs(context: Context) {
     const dogs = await DogModel.find();
     //     const dogs : Array<Dog> = [
     //     {name:"Sammy", age: 2},
@@ -46,13 +81,12 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     //     {name:"Butch", age: 4},
     //     {name:"Azuros", age: 9}
     // ];
-
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: {dogs},
-        header: {'Content-Type': 'application/json'}
+        body: { dogs },
+        header: { 'Content-Type': 'application/json' }
     };
-
-};
+}
 
 export default httpTrigger;
+
--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,23 @@ const dogs = new Array<Dog>();
 function displayDogs(){
     dogs.forEach((dog)=>
     {
-        const dogUI = document.createElement("a" );
-        dogUI.href="#";
-        dogUI.className="list-group-item list-group-item-action";
-        dogUI.text = dog.name;
-        dogUI.addEventListener('click',()=> {
-            alert(dog.name+ " "+dog.age);
-        
-    });
-        document.getElementById("dogs-list").append(dogUI);
+        displayDog(dog);
     } );
 }
 
+function displayDog(dog:Dog)
+{
+    const dogUI = document.createElement("a" );
+    dogUI.href="#";
+    dogUI.className="list-group-item list-group-item-action";
+    dogUI.text = dog.name;
+    dogUI.addEventListener('click',()=> {
+        alert(dog.name+ " "+dog.age);
+    
+        });
+    document.getElementById("dogs-list").append(dogUI);
+
+}
 async function loadDogs() {
     const response = await fetch('/api/dogs');
     const json = await response.json();
@@ -32,7 +37,25 @@ async function main() {
     await loadDogs();
     displayDogs();
 }
+document.getElementById("dog-register").addEventListener("click",async  ()=>{
+    const dog = {
+        name:(document.getElementById("dog-name") as HTMLInputElement).value,
+        age:(document.getElementById("dog-age") as HTMLInputElement).value,
+    }
+    const serverResponse = await fetch("/api/dogs", {
+        method:"POST",
+        body:JSON.stringify(dog),
+        headers: { 
+            'Content-Type': 'application/json'
+         }
+    }
 
+    );
+    const serverDog = await serverResponse.json() as Dog;
+    (document.getElementById("dog-name") as HTMLInputElement).value="";
+    (document.getElementById("dog-age") as HTMLInputElement).value="";
+displayDog(serverDog);
+});
 main();
 
 
